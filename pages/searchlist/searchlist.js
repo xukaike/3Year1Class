072f343c17b7403fbd89d3e7a2a101f0349884e2
xukaike <1354385166@qq.com>
2019-05-27 21:58:04 +0800

    更新下拉加载

--- pages/searchlist/searchlist.js
+++ pages/searchlist/searchlist.js
@@ -3,7 +3,8 @@ Page({
   data: {
     searchvalue:'',
     searchresult:[],
-    searchitem:''
+    searchitem:'',
+    fragment:0
   },
   onLoad(option){
     var that = this;
@@ -16,7 +17,7 @@ Page({
       url: 'https://sv.icewhite.cn:9301/page_key_search',
       data: {
         key: that.data.searchvalue,
-        fragment: 1
+        fragment: that.data.fragment
       },
       method:"POST",
       header: {
@@ -30,12 +31,42 @@ Page({
       }
     });
   },
-  back:function(){
+  onReachBottom(){
+    var that = this;
+    this.data.fragment++;
+    console.log('bottom','fragment='+this.data.fragment)
+    wx.showLoading({
+      title:'加载中'
+    })
+    wx.request({
+      url: 'https://sv.icewhite.cn:9301/page_key_search',
+      data: {
+        key: that.data.searchvalue,
+        fragment: that.data.fragment
+      },
+      method:"POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' 
+      },
+      success(res){
+        let list = that.data.searchresult;
+        for(var i=0;i<res.data.result.length;i++){
+          list.push(res.data.result[i])
+        }
+        that.setData({
+          searchresult:list
+        })
+        wx.hideLoading()
+        console.log(that.data.searchresult)
+      }
+    })
+  },
+  back(){
     wx.navigateBack({
       delta: 2
     })
   },
-  toarticle:function(e){
+  toarticle(e){
     
     var obj = e.currentTarget.dataset.obj;
     let item = escape(JSON.stringify(obj));
