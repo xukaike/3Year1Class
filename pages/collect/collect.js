fb2e98af57fbe902794610ac561b07d44d71279d
xukaike <1354385166@qq.com>
2019-05-29 16:34:04 +0800

    完成收藏列表、删除功能

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,22 +1,45 @@
 const app = getApp();
+
+var utils = require('../../utils/util.js');
 Page({
   data: {
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
-    Custom: app.globalData.Custom,
-    TabCur: 0,
-    VerticalNavTop: 0
+    list:[],
+    fragment:0
   },
-  tabSelect(e) {
-    this.setData({
-      TabCur: e.currentTarget.dataset.id,
-      VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
-    })
-  },
-  VerticalMain(e) {
-    console.log(e.detail);
-  },
-  onLoad: function () { },
+  onLoad: function () {
+    var that = this;
+    wx.request({
+      url: 'https://sv.icewhite.cn:9301/user_collections',
+      data: {
+        userid:app.globalData.openId,
+        fragment:that.data.fragment
+      },
+      method:"POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' 
+      },
+      success(res){
+        if(res.statusCode == 400){
+          wx.showToast({
+            title:'获取收藏列表失败',
+            icon:'none'
+          })
+        }
+        else{
+          that.setData({
+            list:res.data.result
+          });
+          for(var i = 0;i<that.data.list.length;i++){
+            that.data.list[i]['time'] = utils.formatTime(that.data.list[i].op_time,'M-D h:m')
+          }
+          that.setData({
+            searchresult :   that.data.list
+          })
+          console.log(that.data.searchresult)
+        }
+      }
+    });
+   },
   
   // ListTouch触摸开始
   ListTouchStart(e) {
@@ -47,4 +70,62 @@ Page({
       ListTouchDirection: null
     })
   },
+  cancelcollection(e){
+    var that =this
+    let id = e.currentTarget.dataset.obj
+    console.log(id)
+    wx.request({
+      url: 'https://sv.icewhite.cn:9301/cancel_star_page',
+      data: {
+        userid:app.globalData.openId,
+        pageid:id
+      },
+      method:"POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' 
+      },
+      success(res){
+        
+        if(res.statusCode == 400){
+          wx.showToast({
+            title:'取消失败！',
+            icon:'none'
+          })
+        }
+        else{
+          wx.request({
+            url: 'https://sv.icewhite.cn:9301/user_collections',
+            data: {
+              userid:app.globalData.openId,
+              fragment:that.data.fragment
+            },
+            method:"POST",
+            header: {
+              'content-type': 'application/x-www-form-urlencoded' 
+            },
+            success(res){
+              if(res.statusCode == 400){
+                wx.showToast({
+                  title:'获取收藏列表失败',
+                  icon:'none'
+                })
+              }
+              else{
+                that.setData({
+                  list:res.data.result
+                });
+                for(var i = 0;i<that.data.list.length;i++){
+                  that.data.list[i]['time'] = utils.formatTime(that.data.list[i].op_time,'M-D h:m')
+                }
+                that.setData({
+                  searchresult :   that.data.list
+                })
+                console.log(that.data.searchresult)
+              }
+            }
+          });
+        }
+      }
+    });
+  }
 });
