2c498f569fbbfad31dec2f7d2490996e658ad791
xukaike <1354385166@qq.com>
2019-06-03 19:28:17 +0800

    完成删除历史记录功能

--- pages/history/history.js
+++ pages/history/history.js
@@ -8,7 +8,8 @@ Page({
     showList:{},
     fragment:0,
     finallList:{},
-    
+    showModal:false,
+    deleteid:''
   },
   onLoad(){
     var that = this;
@@ -23,39 +24,7 @@ Page({
         'content-type': 'application/x-www-form-urlencoded' 
       },
       success(res){
-        that.setData({
-          list:res.data.result
-        })
-        console.log(that.data.list)
-        let array = []
-
-
-        for(var i=0;i<that.data.list.length;i++){
-
-          that.data.list[i]['time'] = utils.formatTime(that.data.list[i].op_time,"h:m")
-
-          if(i!=0){
-            if(utils.formatTime(that.data.list[i].op_time,"M-D")==utils.formatTime(that.data.list[i-1].op_time,"M-D")){
-              array.push(that.data.list[i])
-            }
-            else{
-              array = []
-              array.push(that.data.list[i])
-            }
-            that.data.showList[utils.formatTime(that.data.list[i].op_time,"M-D")]=array
-          }
-          else{
-            array.push(that.data.list[i])
-            that.data.showList[utils.formatTime(that.data.list[i].op_time,"M-D")]=array
-            console.log(i=0)
-          }
-        }
-
-
-        that.setData({
-          finallList:that.data.showList
-        })
-        console.log(that.data.finallList)
+        postList(that, res);
       }
     });
 
@@ -68,33 +37,112 @@ Page({
       url:'../article/article?item='+id
     })
   },
-  // ListTouch触摸开始
-  ListTouchStart(e) {
+  showModal(e){
+    let id = e.currentTarget.dataset.id
+    console.log(id)
     this.setData({
-      ListTouchStart: e.touches[0].pageX
+      showModal:true,
+      deleteid:id
     })
   },
-
-  // ListTouch计算方向
-  ListTouchMove(e) {
+  hideModal(){
     this.setData({
-      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+      showModal:false
     })
   },
-
-  // ListTouch计算滚动
-  ListTouchEnd(e) {
-    if (this.data.ListTouchDirection =='left'){
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    } else {
-      this.setData({
-        modalName: null
-      })
-    }
-    this.setData({
-      ListTouchDirection: null
+  delete(){
+    var that = this;
+    wx.request({
+      url:'https://sv.icewhite.cn:9301/delete_history',
+      data: {
+        userid: app.globalData.openId,
+        pageid: this.data.deleteid
+      },
+      method:"POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' 
+      },
+      success:res=>{
+        if(res.statusCode == 400){
+          wx.showToast({
+            title:"删除失败",
+            icon:'none'
+          })
+        }
+        else{
+          wx.request({
+            url: 'https://sv.icewhite.cn:9301/browser_history',
+            data: {
+              userid: app.globalData.openId,
+              fragment: this.data.fragment
+            },
+            method:"POST",
+            header: {
+              'content-type': 'application/x-www-form-urlencoded' 
+            },
+            success(res){
+              that.setData({
+                list: res.data.result
+              });
+              console.log(that.data.list);
+              let array = [];
+              for (var i = 0; i < that.data.list.length; i++) {
+                that.data.list[i]['time'] = utils.formatTime(that.data.list[i].op_time, "h:m");
+                if (i != 0) {
+                  if (utils.formatTime(that.data.list[i].op_time, "M-D") == utils.formatTime(that.data.list[i - 1].op_time, "M-D")) {
+                    array.push(that.data.list[i]);
+                  }
+                  else {
+                    array = [];
+                    array.push(that.data.list[i]);
+                  }
+                  that.data.showList[utils.formatTime(that.data.list[i].op_time, "M-D")] = array;
+                }
+                else {
+                  array.push(that.data.list[i]);
+                  that.data.showList[utils.formatTime(that.data.list[i].op_time, "M-D")] = array;
+                  console.log(i = 0);
+                }
+              }
+              that.setData({
+                finallList: that.data.showList,
+                showModal:false
+              });
+              console.log(that.data.finallList);
+            }
+          })
+        }
+      }
     })
-  },
+  }
 });
+
+function postList(that, res) {
+  that.setData({
+    list: res.data.result
+  });
+  console.log(that.data.list);
+  let array = [];
+  for (var i = 0; i < that.data.list.length; i++) {
+    that.data.list[i]['time'] = utils.formatTime(that.data.list[i].op_time, "h:m");
+    if (i != 0) {
+      if (utils.formatTime(that.data.list[i].op_time, "M-D") == utils.formatTime(that.data.list[i - 1].op_time, "M-D")) {
+        array.push(that.data.list[i]);
+      }
+      else {
+        array = [];
+        array.push(that.data.list[i]);
+      }
+      that.data.showList[utils.formatTime(that.data.list[i].op_time, "M-D")] = array;
+    }
+    else {
+      array.push(that.data.list[i]);
+      that.data.showList[utils.formatTime(that.data.list[i].op_time, "M-D")] = array;
+      console.log(i = 0);
+    }
+  }
+  that.setData({
+    finallList: that.data.showList
+  });
+  console.log(that.data.finallList);
+}
