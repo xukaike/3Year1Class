658492b5d148e6d2d45265f96ad07b7428fbaa19
xukaike <1354385166@qq.com>
2019-06-16 16:07:54 +0800

    增加滑动动画

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -4,22 +4,10 @@ Page({
     TabCur: 0,
     scrollLeft: 0,
     title:'热点',
-    loadcount:0
+    loadcount:0,
+    isLoad:false
   },
-  tabSelect(e) {
-    wx.showLoading({
-      title:'加载中'
-    })
-    var that =this;
-    console.log(e);
-    this.setData({
-      TabCur: e.currentTarget.dataset.id,
-      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
-    })
-    console.log(this.data.tag[that.data.TabCur])
-    
-    switchTabPost(that);//切换Tab
-  },
+  
   onLoad(){
     var that = this;
     wx.request({
@@ -38,49 +26,46 @@ Page({
           })
         }
         else{
-        that.setData({
-          tag:res.data.user_tags
-        })
-        wx.showLoading({
-          title: '加载中',
-        })
-        wx.request({
-          url:'https://sv.icewhite.cn:9301/tag_pages',
-          method:'POST',
-          header: {
-            'content-type': 'application/x-www-form-urlencoded' 
-          },
-          data:{
-            userid:app.globalData.openId,
-            tag:that.data.tag[0]
-          },
-          success:res=>{
-            if(res.statusCode == 400){
-              wx.showToast({
-                title:'加载文章失败！'
-              })
-            }
-            else{
-              that.setData({
-                pages:res.data.result
-              })
-              wx.hideLoading()
-              console.log(that.data.pages)
-            }
+          let allpagelist = []
+          let page = []
+          for(var tag in res.data.user_tags){
+            let pageitem = {}
+            pageitem[res.data.user_tags[tag]] = page 
+            pageitem['isLoad'] = false
+            allpagelist[tag] = pageitem
           }
-        })
+          that.setData({
+            tag:res.data.user_tags,
+          })
+          wx.request({
+            url:'https://sv.icewhite.cn:9301/tag_pages',
+            method:'POST',
+            header: {
+              'content-type': 'application/x-www-form-urlencoded' 
+            },
+            data:{
+              userid:app.globalData.openId,
+              tag:that.data.tag[0] //第一个标签
+            },
+            success:res=>{
+              if(res.statusCode == 400){
+                wx.showToast({
+                  title:'加载文章失败！'
+                })
+              }
+              else{
+                allpagelist[0][that.data.tag[0]] = res.data.result
+                allpagelist[0]['isLoad'] = true
+                that.setData({
+                  allpagelist:allpagelist
+                })
+              }
+            }
+          })
         }
       }
     })
   },
-  toarticle(e){
-    let item = escape(JSON.stringify(e.currentTarget.dataset.obj))
-    console.log(item)
-    wx.navigateTo({
-      url:'../article/article?item='+item
-    })
-  },
-  
   onShow(){
     if(this.data.loadcount == 0){
      this.data.loadcount++ 
@@ -89,14 +74,6 @@ Page({
       this.reLoad();//构造reLoad能同步标签
       console.log('reload')
     }
-  },
-  tosettag(){
-    this.setData({
-      modalName: ''
-    })
-    wx.navigateTo({
-      url:'../tag/tag'
-    })
   },
   reLoad(){
     var that = this;
@@ -110,18 +87,92 @@ Page({
         userid:app.globalData.openId
       },
       success:res=>{
-        if(res.data.user_tags.length == 0){
+        if(res.data.user_tags.length == 0){//如果用户没有标签
           that.setData({
             modalName: 'DialogModal2'
           })
         }
-        else{
-          
-        that.setData({
-          tag:res.data.user_tags
-        })
-        wx.request({
-          url:'https://sv.icewhite.cn:9301/tag_pages',
+        else if(that.data.tag.toString() != res.data.user_tags.toString()){
+          let allpagelist = []
+          let page = []
+          for(var tag in res.data.user_tags){
+            let pageitem = {}
+            pageitem[res.data.user_tags[tag]] = page 
+            pageitem['isLoad'] = false
+            allpagelist[tag] = pageitem
+          }
+          that.setData({
+            tag:res.data.user_tags,
+            TabCur:0
+          })
+          wx.request({
+            url:'https://sv.icewhite.cn:9301/tag_pages',
+            method:'POST',
+            header: {
+              'content-type': 'application/x-www-form-urlencoded' 
+            },
+            data:{
+              userid:app.globalData.openId,
+              tag:that.data.tag[0] //第一个标签
+            },
+            success:res=>{
+              if(res.statusCode == 400){
+                wx.showToast({
+                  title:'加载文章失败！'
+                })
+              }
+              else{
+                allpagelist[0][that.data.tag[0]] = res.data.result
+                allpagelist[0]['isLoad'] = true
+                that.setData({
+                  allpagelist:allpagelist
+                })
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  tabSelect(e) {
+    var that =this;
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60,
+      isLoad:false
+    })
+    
+  },
+  toarticle(e){
+    let item = escape(JSON.stringify(e.currentTarget.dataset.obj))
+    wx.navigateTo({
+      url:'../article/article?item='+item
+    })
+  },
+  
+  
+  tosettag(){
+    this.setData({
+      modalName: ''
+    })
+    wx.navigateTo({
+      url:'../tag/tag'
+    })
+  },
+  
+  bindChange(e){
+    var that = this
+    this.setData({
+      TabCur:e.detail.current,
+      scrollLeft: (e.detail.current - 1) * 60,
+      isLoad:false //防止快速切换页面还在显示加载中
+    })
+    if(this.data.allpagelist[e.detail.current]['isLoad'] == false){
+      that.setData({
+        isLoad:true
+      })
+      wx.request({
+        url:'https://sv.icewhite.cn:9301/tag_pages',
           method:'POST',
           header: {
             'content-type': 'application/x-www-form-urlencoded' 
@@ -137,72 +188,16 @@ Page({
               })
             }
             else{
+              that.data.allpagelist[e.detail.current][that.data.tag[that.data.TabCur]] = res.data.result
+              that.data.allpagelist[e.detail.current]['isLoad'] = true
               that.setData({
-                pages:res.data.result
+                allpagelist:that.data.allpagelist,
+                isLoad:false
               })
-              console.log(that.data.pages)
+              wx.hideLoading()
             }
           }
-        })
-        }
-      }
-    })
-  },
-  touchStart(e) {
-    this.setData({
-      "touch.x": e.changedTouches[0].clientX,
-      "touch.y": e.changedTouches[0].clientY
-    });
-  },
-  touchEnd(e) {
-    var that = this;
-    let endX = e.changedTouches[0].clientX;
-    let endY = e.changedTouches[0].clientY;
-    
-    if (endX - this.data.touch.x > 100 && Math.abs(endY - this.data.touch.y) < 100) {      //右滑
-       
-      if(this.data.TabCur!=0){
-        wx.showLoading({
-          title:'加载中'
-        })
-        this.setData({
-          TabCur:this.data.TabCur-1,
-          scrollLeft: (this.data.TabCur - 1) * 60
-        })
-        switchTabPost(that);
-      }
-    } else if (endX - this.data.touch.x < -100 && Math.abs(endY - this.data.touch.y) < 100) {   //左滑
-      
-      if(this.data.TabCur!=this.data.tag.length-1){
-        wx.showLoading({
-          title:'加载中'
-        })
-        this.setData({
-          TabCur:this.data.TabCur+1,
-          scrollLeft: (this.data.TabCur - 1) * 60
-        })
-        switchTabPost(that);
-      }
+      })
     }
   }
 })
-
-function switchTabPost(that) {
-  wx.request({
-    url: 'https://sv.icewhite.cn:9301/tag_pages',
-    method: 'POST',
-    header: {
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    data: {
-      userid: app.globalData.openId,
-      tag: that.data.tag[that.data.TabCur]
-    },
-    success: res => {
-      that.setData({
-        pages: res.data.result
-      });
-      wx.hideLoading();
-    }
-  });
-}
